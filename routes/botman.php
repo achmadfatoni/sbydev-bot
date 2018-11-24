e2a46c63ded677031e99e72763fa380d41965c48
Achmad Fatoni <achmadfatony@gmail.com>
2018-11-24 10:46:49 +0700

    register /events command

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,11 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
+$botman->hears('/events', function ($bot) {
+   $bot->reply('Lagi dibikin gan, tapi entah kapan selesainya. sabar dulu ya!!!');
+});
+
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+//$botman->hears('Start conversation', BotManController::class.'@startConversation');
